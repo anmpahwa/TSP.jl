63a7a12622f9951581ef93da41f7866afbf1eb60
anmol1104 <anmpahwa@ucdavis.edu>
2022-08-14 23:35:36 +0530

    update exception handling

--- src/initialize.jl
+++ src/initialize.jl
@@ -22,7 +22,8 @@ function cw_init(rng::AbstractRNG, G)
     d = N[1]
     # Step 1: Initialize solution with routes to every node from the depot node (first node)
     K = length(N)
-    for k ∈ K:-1:1 insertnode!(N[k], d, d, s, ignore=true) end
+    D = [deepcopy(d) for _ ∈ 1:K]
+    for k ∈ K:-1:1 insertnode!(N[k], D[k], D[k], s) end
     # Step 2: Merge routes iteratively until single route traversing all nodes remains
     x = fill(-Inf, (K,K))       # x[i,j]: Savings from merging route with tail node N[i] into route with tail node N[j]
     ϕ = ones(Int64, K)          # ϕ[k]  : if isone(ϕ[k]) implies route k is active else inactive
@@ -31,57 +32,62 @@ function cw_init(rng::AbstractRNG, G)
         z = f(s)
         for i ∈ 2:K
             n₁ = N[i]
-            if !isequal(n₁.h, d.i) continue end
+            d₁ = D[i]
+            if !isequal(n₁.h, d₁.i) continue end
             for j ∈ 2:K
                 # Step 2.1.1: Merge routes with tail node N[i] into route with tail node N[j]
                 n₂ = N[j]
+                d₂ = D[j]
                 if isequal(n₁, n₂) continue end
-                if !isequal(n₂.h, d.i) continue end
+                if !isequal(n₂.h, d₂.i) continue end
                 if iszero(ϕ[i]) & iszero(ϕ[j]) continue end
-                q  = d
+                nₕ = d₂
                 nₒ = n₁
-                p  = N[nₒ.t]
+                nₜ = isone(nₒ.t) ? d₁ : N[nₒ.t]
                 while true
-                    removenode!(nₒ, p, d, s, ignore=true)
-                    insertnode!(nₒ, n₂,q, s, ignore=true)
-                    if isequal(p, d) break end
-                    q  = nₒ
-                    nₒ = p
-                    p  = N[nₒ.t]
+                    removenode!(nₒ, nₜ, d₁, s)
+                    insertnode!(nₒ, n₂, nₕ, s)
+                    if isequal(nₜ, d₁) break end
+                    nₕ = nₒ
+                    nₒ = nₜ
+                    nₜ = isone(nₒ.t) ? d₁ : N[nₒ.t]
                 end
                 # Step 2.1.2: Compute savings from merging route with tail node N[i] into route with tail node N[j]
                 z⁻ = f(s)
                 Δ  = z - z⁻
                 x[i,j] = Δ
                 # Step 2.1.3: Unmerge routes with tail node N[i] into route with tail node N[j]
-                p  = d
+                nₜ = d₁
                 nₒ = N[n₂.h]
-                q  = N[nₒ.h]
+                nₕ = isone(nₒ.h) ? d₂ : N[nₒ.h]
                 while true
-                    removenode!(nₒ, n₂,q, s, ignore=true)
-                    insertnode!(nₒ, p, d, s, ignore=true)
-                    if isequal(q, d) break end
-                    p  = nₒ
-                    nₒ = q
-                    q  = N[nₒ.h]
+                    removenode!(nₒ, n₂, nₕ, s)
+                    insertnode!(nₒ, nₜ, d₁, s)
+                    if isequal(nₕ, d₂) break end
+                    nₜ = nₒ
+                    nₒ = nₕ
+                    nₕ = isone(nₒ.h) ? d₂ : N[nₒ.h]
                 end
             end
         end
         # Step 2.2: Merge routes that render highest savings
         i,j = Tuple(argmax(x))
         n₁ = N[i]
+        d₁ = D[i]
         n₂ = N[j]
-        q  = d
+        d₂ = D[j]
+        nₕ = d₂
         nₒ = n₁
-        p  = N[nₒ.t]
+        nₜ = isone(nₒ.t) ? d₁ : N[nₒ.t]
         while true
-            removenode!(nₒ, p, d, s, ignore=true)
-            insertnode!(nₒ, n₂,q, s, ignore=true)
-            if isequal(p, d) break end
-            q  = nₒ
-            nₒ = p
-            p  = N[nₒ.t]
+            removenode!(nₒ, nₜ, d₁, s)
+            insertnode!(nₒ, n₂, nₕ, s)
+            if isequal(nₜ, d₁) break end
+            nₕ = nₒ
+            nₒ = nₜ
+            nₜ = isone(nₒ.t) ? d₁ : N[nₒ.t]
         end
+        D[i] = D[j]
         # Step 2.3: Revise savings and selection vectors appropriately
         x[i, :] .= -Inf
         x[:, i] .= -Inf
--- src/operations.jl
+++ src/operations.jl
@@ -2,10 +2,11 @@
     insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
 
 Insert node `nₒ` between tail node `nₜ` and head node `nₕ` in solution `s`.
-Ignores argument errors if ignore is true.
 """
-function insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
-    if !ignore if isclose(nₒ) throw(ArgumentError("Node nₒ (node $(nₒ.i)) is closed")) end end
+function insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
+    if isclose(nₒ) throw(ArgumentError("Node nₒ ($(nₒ.i)) is closed")) end
+    if !isequal(nₜ.h, nₕ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) != Index of head node nₕ ($(nₕ.i)) ")) end            
+    if !isequal(nₕ.t, nₜ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) != Index of tail node nₜ ($(nₜ.i)) ")) end
     A  = s.A 
     aₒ = A[(nₜ.i, nₕ.i)]
     aₜ = A[(nₜ.i, nₒ.i)]
@@ -22,15 +23,12 @@ end
     removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
 
 Remove node `nₒ` from its position between tail node `nₜ` and head node `nₕ` in solution `s`.
-Ignores argument errors if ignore is true.
 """
-function removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
-    if !ignore
-        if !isequal(nₒ.t, nₜ.i) throw(ArgumentError("Tail index of node nₒ (node $(nₒ.i)) != Index of tail node nₜ (node $(nₜ.i))")) end
-        if !isequal(nₜ.h, nₒ.i) throw(ArgumentError("Head index of tail node nₜ (node $(nₜ.i)) != Index of node nₒ (node $(nₒ.i))")) end
-        if !isequal(nₒ.h, nₕ.i) throw(ArgumentError("Head index of node nₒ (node $(nₒ.i)) != Index of head node nₕ (node $(nₕ.i))")) end
-        if !isequal(nₕ.t, nₒ.i) throw(ArgumentError("Tail index of head node nₕ (node $(nₕ.i)) != Index of node nₒ (node $(nₒ.i))")) end
-    end
+function removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
+    if !isequal(nₒ.t, nₜ.i) throw(ArgumentError("Tail index of node nₒ ($(nₒ.i)) != Index of tail node nₜ ($(nₜ.i))")) end
+    if !isequal(nₜ.h, nₒ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) != Index of node nₒ ($(nₒ.i))")) end
+    if !isequal(nₒ.h, nₕ.i) throw(ArgumentError("Head index of node nₒ ($(nₒ.i)) != Index of head node nₕ ($(nₕ.i))")) end
+    if !isequal(nₕ.t, nₒ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) != Index of node nₒ ($(nₒ.i))")) end
     A  = s.A 
     aₒ = A[(nₜ.i, nₕ.i)]
     aₜ = A[(nₜ.i, nₒ.i)]
