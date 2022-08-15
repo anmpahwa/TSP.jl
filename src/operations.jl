805a4c1582695ff3a58bca68247368636ab1f7df
anmol1104 <anmpahwa@ucdavis.edu>
2022-08-15 12:39:08 +0530

    simplify operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -5,8 +5,8 @@ Insert node `nₒ` between tail node `nₜ` and head node `nₕ` in solution `s`
 """
 function insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
     if isclose(nₒ) throw(ArgumentError("Node nₒ ($(nₒ.i)) is closed")) end
-    if !isequal(nₜ.h, nₕ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) != Index of head node nₕ ($(nₕ.i)) ")) end            
-    if !isequal(nₕ.t, nₜ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) != Index of tail node nₜ ($(nₜ.i)) ")) end
+    if !isequal(nₜ.h, nₕ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) does not match the index of head node nₕ ($(nₕ.i)) ")) end            
+    if !isequal(nₕ.t, nₜ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) does not match the index of tail node nₜ ($(nₜ.i)) ")) end
     A  = s.A 
     aₒ = A[(nₜ.i, nₕ.i)]
     aₜ = A[(nₜ.i, nₒ.i)]
@@ -25,16 +25,16 @@ end
 Remove node `nₒ` from its position between tail node `nₜ` and head node `nₕ` in solution `s`.
 """
 function removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
-    if !isequal(nₒ.t, nₜ.i) throw(ArgumentError("Tail index of node nₒ ($(nₒ.i)) != Index of tail node nₜ ($(nₜ.i))")) end
-    if !isequal(nₜ.h, nₒ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) != Index of node nₒ ($(nₒ.i))")) end
-    if !isequal(nₒ.h, nₕ.i) throw(ArgumentError("Head index of node nₒ ($(nₒ.i)) != Index of head node nₕ ($(nₕ.i))")) end
-    if !isequal(nₕ.t, nₒ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) != Index of node nₒ ($(nₒ.i))")) end
+    if !isequal(nₒ.t, nₜ.i) throw(ArgumentError("Tail index of node nₒ ($(nₒ.i)) does not match the index of tail node nₜ ($(nₜ.i))")) end
+    if !isequal(nₜ.h, nₒ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) does not match the index of node nₒ ($(nₒ.i))")) end
+    if !isequal(nₒ.h, nₕ.i) throw(ArgumentError("Head index of node nₒ ($(nₒ.i)) does not match the index of head node nₕ ($(nₕ.i))")) end
+    if !isequal(nₕ.t, nₒ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) does not match the index of node nₒ ($(nₒ.i))")) end
     A  = s.A 
     aₒ = A[(nₜ.i, nₕ.i)]
     aₜ = A[(nₜ.i, nₒ.i)]
     aₕ = A[(nₒ.i, nₕ.i)]
-    nₜ.h = nₒ.h
-    nₕ.t = nₒ.t
+    nₜ.h = nₕ.i
+    nₕ.t = nₜ.i
     nₒ.t = nₒ.i
     nₒ.h = nₒ.i
     s.c -= aₜ.c + aₕ.c - aₒ.c
