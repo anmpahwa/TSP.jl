918458949d8cc4039f265b87a3c73bfeb50de79b
anmol1104 <anmpahwa@ucdavis.edu>
2022-08-21 16:15:39 +0530

    simple operations

--- src/operations.jl
+++ src/operations.jl
@@ -1,16 +1,12 @@
 """
-    insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
+    insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
 
 Insert node `nₒ` between tail node `nₜ` and head node `nₕ` in solution `s`.
 """
 function insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
-    if isclose(nₒ) throw(ArgumentError("Node nₒ ($(nₒ.i)) is closed")) end
-    if !isequal(nₜ.h, nₕ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) does not match the index of head node nₕ ($(nₕ.i)) ")) end            
-    if !isequal(nₕ.t, nₜ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) does not match the index of tail node nₜ ($(nₜ.i)) ")) end
-    A  = s.A 
-    aₒ = A[(nₜ.i, nₕ.i)]
-    aₜ = A[(nₜ.i, nₒ.i)]
-    aₕ = A[(nₒ.i, nₕ.i)]
+    aₒ = s.A[(nₜ.i, nₕ.i)]
+    aₜ = s.A[(nₜ.i, nₒ.i)]
+    aₕ = s.A[(nₒ.i, nₕ.i)]
     nₜ.h = nₒ.i
     nₕ.t = nₒ.i
     nₒ.t = nₜ.i
@@ -20,19 +16,14 @@ function insertnode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
 end
 
 """
-    removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution; ignore=false)
+    removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
 
 Remove node `nₒ` from its position between tail node `nₜ` and head node `nₕ` in solution `s`.
 """
 function removenode!(nₒ::Node, nₜ::Node, nₕ::Node, s::Solution)
-    if !isequal(nₒ.t, nₜ.i) throw(ArgumentError("Tail index of node nₒ ($(nₒ.i)) does not match the index of tail node nₜ ($(nₜ.i))")) end
-    if !isequal(nₜ.h, nₒ.i) throw(ArgumentError("Head index of tail node nₜ ($(nₜ.i)) does not match the index of node nₒ ($(nₒ.i))")) end
-    if !isequal(nₒ.h, nₕ.i) throw(ArgumentError("Head index of node nₒ ($(nₒ.i)) does not match the index of head node nₕ ($(nₕ.i))")) end
-    if !isequal(nₕ.t, nₒ.i) throw(ArgumentError("Tail index of head node nₕ ($(nₕ.i)) does not match the index of node nₒ ($(nₒ.i))")) end
-    A  = s.A 
-    aₒ = A[(nₜ.i, nₕ.i)]
-    aₜ = A[(nₜ.i, nₒ.i)]
-    aₕ = A[(nₒ.i, nₕ.i)]
+    aₒ = s.A[(nₜ.i, nₕ.i)]
+    aₜ = s.A[(nₜ.i, nₒ.i)]
+    aₕ = s.A[(nₒ.i, nₕ.i)]
     nₜ.h = nₕ.i
     nₕ.t = nₜ.i
     nₒ.t = nₒ.i
