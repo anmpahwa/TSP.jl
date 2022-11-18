01f6199d8fde5029e44afdff1eaee7b927a40b94
anmol1104 <anmpahwa@ucdavis.edu>
2022-11-18 02:58:03 -0800

    update example

--- src/example.jl
+++ src/example.jl
@@ -63,8 +63,8 @@ let
     # Visualize best solution   
     display(visualize(s⃰))
     # Animate ALNS solution search process from inital to best solution
-    #display(animate(S))
+    display(animate(S))
     # Show convergence plot
-    #display(pltcnv(S))
+    display(pltcnv(S))
     return
 end
