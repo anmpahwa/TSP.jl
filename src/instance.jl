e7d44f8f7dc0939dedaf11dd8c1613d8d39b7bb5
anmol1104 <anmpahwa@ucdavis.edu>
2023-02-21 17:54:21 -0800

    update instance.jl

--- src/instance.jl
+++ src/instance.jl
@@ -1,33 +1,18 @@
 # Builds instance as graph with set of nodes and arcs.
 function build(instance; root=joinpath(dirname(@__DIR__), "instances"))
-    f = joinpath(root, "$instance.csv")
+    # Nodes
+    f = joinpath(root, "$instance/nodes.csv")
     csv = CSV.File(f, types=[Int64, Float64, Float64])
     df = DataFrame(csv)
     K = nrow(df)
-    # Nodes
     N = Vector{Node}(undef, K)
-    for k ∈ 1:K
-        i = df[k,1]::Int64
-        x = df[k,2]::Float64
-        y = df[k,3]::Float64
-        n = Node(i, x, y)
-        N[i] = n
-    end
+    for k ∈ 1:K N[k] = Node(df[k,1], df[k,2], df[k,3]) end
     # Arcs
+    f = joinpath(root, "$instance/arcs.csv")
+    csv = CSV.File(f, header=false)
+    df = DataFrame(csv)
     A = Dict{Tuple{Int64,Int64},Arc}()
-    for p ∈ 1:K
-        i  = df[p,1]::Int64
-        xᵢ = df[p,2]::Float64
-        yᵢ = df[p,3]::Float64
-        for q ∈ 1:K
-            j  = df[q,1]::Int64
-            xⱼ = df[q,2]::Float64
-            yⱼ = df[q,3]::Float64
-            c  = sqrt((xⱼ - xᵢ)^2 + (yⱼ - yᵢ)^2)
-            a  = Arc(i, j, c)
-            A[(i,j)] = a
-        end
-    end
+    for i ∈ 1:K for j ∈ 1:K A[(i,j)] = Arc(i, j, df[i,j]) end end
     G = (N, A)
     return G
 end
