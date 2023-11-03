f26a6dd3aa4b842267288cef2062ae5ee5dc9799
anmol1104 <anmpahwa@ucdavis.edu>
2023-11-02 20:18:46 -0700

    update isfeasible

--- src/functions.jl
+++ src/functions.jl
@@ -68,19 +68,6 @@ f(s::Solution) = s.c
 """
     isfeasible(s::Solution)
 
-Returns `true` if node service constraint, node flow constraint, and
-sub-tour elimination constraint are not violated.
+Returns `true` if all nodes are served on the route.
 """
-function isfeasible(s::Solution) 
-    N  = s.N
-    X  = zeros(Int64, length(N))
-    nₒ = N[1]
-    while true
-        k = nₒ.i
-        if isone(X[k]) break end
-        X[k] = 1
-        nₒ = N[nₒ.h]
-    end
-    if any(iszero, X) return false end
-    return true
-end
+isfeasible(s::Solution) = all(isclose, s.N)
